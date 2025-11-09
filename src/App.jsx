85d2658b5b68fa8c07a84d15e8f31cec7267f22d
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-11-09 22:40:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ApplicationForm from './components/ApplicationForm.jsx'
 import ConsentForm from './components/ConsentForm.jsx'
+ import ConsentFormPro from './components/ConsentFormPro.jsx
 import ApplicationFormOfficial from './components/ApplicationFormOfficial.jsx'
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <h1 className='text-3xl font-bold text-red-600 mb-6 text-center'>THUSANANG DIGITAL SYSTEM</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
         <ApplicationFormOfficial/>
-        <ConsentForm />
+        <ConsentFormPro/>
       </div>
     </div>
   )
